f2a05b84cb4c675bc9f5f80718f3e8996e764f23
Sergey Popov <beast_sergey@mail.ru>
2014-10-18 10:17:28 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+SampleUserManagement by Beast
---
+++ app-core/src/main/java/com/epam/brest/courses/domain/User.java
@@ -0,0 +1,31 @@
+package com.epam.brest.courses.domain;
+
+public class User {
+	private Long userId;
+	private String login;
+	private String name;
+
+	public Long getUserId() {
+		return userId;	
+	}
+
+	public void setUserId(Long userID) {
+		this.userId = userId;	
+	}
+
+	public getLogin() {
+		return login;	
+	}
+
+	public setLogin(String login) {
+		this.login = login;
+	}
+
+	public getName() {
+		return name;	
+	}
+
+	public setName(String name) {
+		this.name = name;	
+	}
+}
