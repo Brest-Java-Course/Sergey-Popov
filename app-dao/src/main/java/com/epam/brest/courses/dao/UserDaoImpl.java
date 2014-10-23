a5ef8c465463576878823aee2ff9d777b22e1648
Sergey Popov <beast_sergey@mail.ru>
2014-10-23 12:09:38 +0300

    spring context and tests added

--- app-dao/src/main/java/com/epam/brest/courses/dao/UserDaoImpl.java
+++ app-dao/src/main/java/com/epam/brest/courses/dao/UserDaoImpl.java
@@ -1,25 +1,50 @@
 package com.epam.brest.courses.dao;
 
 import com.epam.brest.courses.domain.User;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 /**
  * Created by beast on 21.10.14.
  */
 public class UserDaoImpl implements UserDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public void setDataSource(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
     @Override
     public void addUser(User user) {
-
+        jdbcTemplate.update("insert into USER (userid, login, name) values (?, ?, ?)",
+            user.getUserId(), user.getLogin(), user.getName());
     }
 
     @Override
     public List<User> getUsers() {
-        return null;
+        return jdbcTemplate.query("select userid, login, name from USER", new UserMapper());
     }
 
     @Override
     public void removeUser(Long userId) {
 
+    }
+
+    private class UserMapper implements RowMapper<User> {
+
+        @Override
+        public User mapRow(ResultSet resultSet, int i) throws SQLException {
+            User user = new User();
+            user.setUserId(resultSet.getLong("userid"));
+            user.setLogin(resultSet.getString("login"));
+            user.setName(resultSet.getString("name"));
+            return user;
+        }
     }
 }
