cf1e09c23dd96cfb78fa71a0ff1f444b43639b0e
Sergey Popov <beast_sergey@mail.ru>
2015-01-29 09:50:45 +0300

    clean project

--- tm-dao/src/test/resources/sql/data-script.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `person` VALUES
-(null, 'testFirstName1', 'testLastName1'), (null, 'testFirstName2', 'testLastName2');
-
-INSERT INTO `task` VALUES
-(null, 'testTaskName1', '2014-11-20 15:30:10', null, null, 1, 1);
--- tm-web-rest/src/main/webapp/WEB-INF/jsp/error/404.jsp
+++
@@ -1,2 +0,0 @@
-<%@ page isErrorPage="true"%>
-Oops! A 404 error happened because the resource could not be found.
